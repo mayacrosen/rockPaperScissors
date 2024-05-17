f104065c17101e6600735d07898c0a5995609a55
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-05-17 13:39:52 -0400

    added playRound

--- rules.js
+++ rules.js
@@ -6,4 +6,54 @@ function getComputerChoice() {
     return choices[index];
 }
 
+function getHumanChoice() {
+    const human = toLowerCase(prompt("Rock, paper, scissors, shoot!"));
+    return human;
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log("You tie. Both chose " + humanChoice);
+        return "tie";
+    }
+    if (humanChoice == "rock") {
+        if (computerChoice == "paper") {
+            console.log("You lose! Paper beats rock");
+            return "computer";
+        }
+        if (computerChoice == "scissors") {
+            console.log("You win! Rock beats scissors");
+            return "player";
+        }
+    }
+    if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            console.log("You win! Paper beats rock");
+            return "player";
+        }
+        if (computerChoice == "scissors") {
+            console.log("You lose! Scissors beat paper");
+            return "computer";
+        }
+    }
+    if (humanChoice == "scissors") {
+        if (computerChoice == "rock") {
+            console.log("You lose! Rock beats scissors");
+            return "computer";
+        }
+        if (computerChoice == "paper") {
+            console.log("You win! Scissors beats paper");
+            return "player";
+        }
+    }
+}
+
+function playGame () {
+    
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+
 console.log(getComputerChoice())
