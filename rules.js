973317a224a57b83fbc7f45c39fb0bc08fc44079
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-06-05 16:34:44 -0400

    deleted extra playRound function

--- rules.js
+++ rules.js
@@ -11,42 +11,6 @@ function getHumanChoice() {
     return human;
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
-        console.log("You tie. Both chose " + humanChoice);
-        return "tie";
-    }
-    if (humanChoice == "rock") {
-        if (computerChoice == "paper") {
-            console.log("You lose! Paper beats rock");
-            return "computer";
-        }
-        if (computerChoice == "scissors") {
-            console.log("You win! Rock beats scissors");
-            return "player";
-        }
-    }
-    if (humanChoice == "paper") {
-        if (computerChoice == "rock") {
-            console.log("You win! Paper beats rock");
-            return "player";
-        }
-        if (computerChoice == "scissors") {
-            console.log("You lose! Scissors beat paper");
-            return "computer";
-        }
-    }
-    if (humanChoice == "scissors") {
-        if (computerChoice == "rock") {
-            console.log("You lose! Rock beats scissors");
-            return "computer";
-        }
-        if (computerChoice == "paper") {
-            console.log("You win! Scissors beats paper");
-            return "player";
-        }
-    }
-}
 
 function playGame () {
     let humanScore = 0;
