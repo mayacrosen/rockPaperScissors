160e40628998916f44c5bcc6f7e78db308699145
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-06-06 08:11:01 -0400

    Working on resetting round
    Fixed the scissors button. Working on reseting round once they reach a certain score, the forEach loop isn't working.

--- rules.js
+++ rules.js
@@ -3,6 +3,7 @@
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const index = Math.floor(Math.random() * 3);
+    computerChoiceDisplay.textContent = "Computer chooses: " + choices[index];
     return choices[index];
 }
 
@@ -14,21 +15,42 @@ function updateScores() {
     score.textContent = "Human: " + humanScore + " Computer: " + computerScore;
 }
 
+
 function addRound(words) {
     const round = document.createElement("li");
     round.textContent = words;
     rounds.appendChild(round);
 }
 
+
+function clearRounds(e) {
+    rounds.removeChild(e);
+}
+
+
 function playRound() {
     const computerChoice = getComputerChoice();
     const humanChoice = this.id;
+
+    if (humanScore >= 5 || computerScore >= 5) {
+        console.log("reset");
+        rounds.childNodes.forEach(clearRounds)
+        if (humanScore > computerScore) {
+            computerChoiceDisplay.textContent = "You win! Computer is sad";
+        }
+        else {
+            computerChoiceDisplay.textContent = "You lose. Computer is happy"
+        }
+        
+    }
+
 
     if (humanChoice == computerChoice) {
         addRound("You tie. Both chose " + humanChoice);
         return "tie";
         }
     if (humanChoice == "rock") {
+        console.log("rock");
         if (computerChoice == "paper") {
             addRound("You lose! Paper beats rock");
             computerScore++;
@@ -44,6 +66,7 @@ function playRound() {
         }
 
     if (humanChoice == "paper") {
+        console.log("paper");
         if (computerChoice == "rock") {
             addRound("You win! Paper beats rock");
             humanScore++;
@@ -58,6 +81,7 @@ function playRound() {
         }
     }
     if (humanChoice == "scissors") {
+        console.log("scissors");
         if (computerChoice == "rock") {
             addRound("You lose! Rock beats scissors");
             computerScore++;
@@ -71,20 +95,16 @@ function playRound() {
             return "player";
         }
     }
-    if (humanScore >= 5 || computerScore >= 5) {
-
-
-    }
+    
 }
 
 
 const score = document.querySelector("div");
-const container = document.querySelector("container");
 const rounds = document.querySelector("ol");
-const computerChoice = document.querySelector("p");
+const computerChoiceDisplay = document.querySelector("p");
 const rockBtn = document.querySelector("#rock");
 const paperBtn = document.querySelector("#paper");
-const scissorsBtn = document.querySelector("scissors");
+const scissorsBtn = document.querySelector("#scissors");
     
 rockBtn.addEventListener("click", playRound);
 paperBtn.addEventListener("click", playRound);
